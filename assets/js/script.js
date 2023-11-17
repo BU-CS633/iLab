da3bf4e81b64b02caa2c0c6a9c06f46467b01351
Andre Arsyan <andrearsyanj@gmail.com>
2023-11-17 15:48:56 -0500

    fix modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,19 +11,20 @@ function getAllRequest() {
     $.ajax(settings).done(function (response) {
         var dataSet = []
         for (let i = 0; i < response.results.length; i++) {
-            dataSet.push([response.results[i].owner.username, response.results[i].item.name, response.results[i].item.vendor, response.results[i].item.catalog, response.results[i].item.channel, response.results[i].item.qty, response.results[i].item.price, response.results[i].request_date])
+            dataSet.push([response.results[i].owner.username, response.results[i].item.name, response.results[i].id, response.results[i].item.vendor, response.results[i].item.catalog, response.results[i].item.channel, response.results[i].item.qty, response.results[i].item.price, response.results[i].request_date])
         }
         new DataTable('#all-request', {
             columns: [
                 { title: 'Owner' },
                 { title: 'Item' },
+                { title: 'Item ID' },
                 { title: 'Vendor' },
                 { title: 'Catalog' },
                 { title: 'Channel' },
                 { title: 'Quantity' },
                 { title: 'Price' },
                 { title: 'Date Request' },
-                { 'data': null, title: 'Action', wrap: true, "render": function (item) { return '<div class="btn-group"> <button type="button" onclick="console.log(\'' + item[0] + ' \')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#DetailModal">Detail</button></div>' } },
+                { 'data': null, title: 'Action', wrap: true, "render": function (item) { return '<div class="btn-group"> <button type="button" onclick="console.log(\'' + item[2] + ' \')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#DetailModal">Detail</button></div>' } },
             ],
             data: dataSet,
         });
@@ -33,6 +34,23 @@ function getAllRequest() {
 function getItemDetail() {
     var settings = {
         "url": "http://127.0.0.1:8000/api/items/1",
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+    };
+
+    $.ajax(settings).done(function (response) {
+        $("#item-name").text(response.name);
+        $("#item-fullname").text(response.fullName);
+        $("#item-vendor").text(response.vendor);
+    });
+}
+
+function getRequestDetail(id) {
+    var settings = {
+        "url": "http://127.0.0.1:8000/api/requests/" + id,
         "method": "GET",
         "timeout": 0,
         "headers": {
