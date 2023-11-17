cd9d2e035f5edd388951923a0227d68d52b0126a
Andre Arsyan <andrearsyanj@gmail.com>
2023-11-17 17:16:52 -0500

    fix url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+const URL = "https://ilab-api.onrender.com"
+
 function getAllRequest() {
     var settings = {
-        "url": "http://127.0.0.1:8000/api/requests/",
+        "url": URL + "/api/requests/",
         "method": "GET",
         "timeout": 0,
         "headers": {
@@ -33,7 +35,7 @@ function getAllRequest() {
 
 function getItemDetail(id) {
     var settings = {
-        "url": "http://127.0.0.1:8000/api/items/" + id,
+        "url": URL + "/api/items/" + id,
         "method": "GET",
         "timeout": 0,
         "headers": {
@@ -50,7 +52,7 @@ function getItemDetail(id) {
 
 function getRequestDetail(id) {
     var settings = {
-        "url": "http://127.0.0.1:8000/api/requests/" + id,
+        "url": URL + "api/requests/" + id,
         "method": "GET",
         "timeout": 0,
         "headers": {
@@ -62,4 +64,51 @@ function getRequestDetail(id) {
         console.log(response)
         console.log(response.item.name)
     });
+}
+
+async function getAllItems() {
+    try {
+        const response = await fetch(URL + "/api/items/");
+        const data = await response.json();
+
+        const dataSet = data.results.map(item => [
+            item.name,
+            item.fullName,
+            item.unitSize ?? 'N/A', // 'N/A' for null values
+            item.qty,
+            item.price ?? 'N/A',
+            item.vendor ?? 'N/A',
+            item.catalog ?? 'N/A',
+            item.lastOrderDate ?? 'N/A',
+            item.lastReceivedDate ?? 'N/A',
+            item.channel ?? 'N/A',
+            item.location ?? 'N/A',
+            item.link ?? 'N/A',
+            item.notes ?? 'N/A',
+            item.id ?? 'N/A'
+        ]);
+
+        $('#inventory-all').DataTable({
+            data: dataSet,
+            columns: [
+                { title: 'Name' },
+                { title: 'Full Name' },
+                { title: 'Unit Size' },
+                { title: 'Quantity' },
+                { title: 'Price' },
+                { title: 'Vendor' },
+                { title: 'Catalog' },
+                { title: 'Last Order Date' },
+                { title: 'Last Received Date' },
+                { title: 'Channel' },
+                { title: 'Location' },
+                { title: 'Link' },
+                { title: 'Notes' },
+                { 'data': null, title: 'Action', wrap: true, "render": function (item) { return '<div class="btn-group"> <button type="button" onclick="console.log(\'' + item[13] + ' \')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#InventoryModal">Detail</button></div>' } },
+
+            ]
+        });
+    } catch (error) {
+        console.error('Error fetching items:', error);
+    }
 }
